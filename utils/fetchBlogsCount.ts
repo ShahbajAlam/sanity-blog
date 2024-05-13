250093bda4906183399e7ae3a515c80c77051ec6
Your Name <you@example.com>
2024-05-13 15:36:33 +0530

    deleted:    utils/fetchBlogs.ts 	deleted:    utils/fetchBlogsCount.ts

--- utils/fetchBlogsCount.ts
+++
@@ -1,9 +0,0 @@
-"use server";
-
-import client from "./client";
-
-export default async function fetchBlogsCount() {
-    const query = `count(*[_type == "blog"])`;
-    const count = client.fetch(query);
-    return count;
-}
